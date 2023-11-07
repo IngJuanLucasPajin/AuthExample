5ccc3cf6db383657f8e52801fb451d00a7d9e9a5
Lucas Pajin <lucas@psiware.com.ar>
2023-11-06 21:18:15 -0300

    Initial commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Model.ViewModel;
+using System.Data;
+using System.Security.Claims;
+
+namespace AuthExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] AuthViewModel user)
+        {
+            //your logic for login process
+            //If login usrename and password are correct then proceed to generate token
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claimsForToken = new List<Claim>();
+            claimsForToken.Add(new Claim("sub", user.username));
+            claimsForToken.Add(new Claim("given_name", user.username));
+            //claimsForToken.Add(new Claim("email", user.Email));
+            //claimsForToken.Add(new Claim("role", user.roles));
+
+            var Sectoken = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              claimsForToken,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            var token = new JwtSecurityTokenHandler().WriteToken(Sectoken);
+
+            return Ok(token);
+        }
+    }
+}
